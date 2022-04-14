b4587179067b92cfa90001fa9ea0868153ea773a
jaeyun-park <wodbs720@naver.com>
2022-04-14 23:09:11 +0900

    Add option in  my to day text form field

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-# My-To-Day
-=======
+
 # my_to_day
 
 A new Flutter project.
@@ -17,4 +15,3 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
->>>>>>> 552cf19 (init commit)
--- lib/widgets/common/my_to_day_text_form_field.dart
+++ lib/widgets/common/my_to_day_text_form_field.dart
@@ -9,22 +9,39 @@ class MyToDayTextFormField extends StatefulWidget {
     this.textStyle,
     this.height,
     this.suffixIcon,
+    this.suffixIconColor,
+    this.onIconPressed,
   });
   final String? hintText;
   final TextStyle? textStyle;
   final double? height;
-  final Widget? suffixIcon;
+  final IconData? suffixIcon;
+  final Color? suffixIconColor;
+  final void Function()? onIconPressed;
 
   @override
   _MyToDayTextFormFieldState createState() => _MyToDayTextFormFieldState();
 }
 
 class _MyToDayTextFormFieldState extends State<MyToDayTextFormField> {
+  final TextEditingController _controller = TextEditingController();
+
+  void handleOnChanged(String value) {
+    _controller.value = TextEditingValue(
+      text: value,
+      selection: TextSelection.fromPosition(
+        TextPosition(offset: value.length),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: widget.height,
       child: TextFormField(
+        controller: _controller,
+        onChanged: handleOnChanged,
         expands: true,
         maxLines: null,
         style: widget.textStyle ??
@@ -34,7 +51,17 @@ class _MyToDayTextFormFieldState extends State<MyToDayTextFormField> {
             ),
         decoration: InputDecoration(
             border: InputBorder.none,
-            suffixIcon: widget.suffixIcon,
+            suffixIcon: IconButton(
+              padding: EdgeInsets.all(10.w),
+              onPressed: widget.onIconPressed,
+              iconSize: 55.w,
+              icon: Icon(
+                widget.suffixIcon ?? Icons.check_box,
+                color: _controller.text.isNotEmpty
+                    ? AppTheme.errorColor
+                    : widget.suffixIconColor ?? AppTheme.backdropOverlay_65,
+              ),
+            ),
             contentPadding: EdgeInsets.all(
               10.w,
             ),
