4b14da9b5fc538d50f7d2356790e9c2ffcdcd9ab
jaeyun-park <wodbs720@naver.com>
2022-04-15 00:07:29 +0900

    Add local storage in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:my_to_day/constants/constants.dart';
 import 'package:my_to_day/routes.dart';
 import 'package:my_to_day/screens/main/init_screen.dart';
+import 'package:my_to_day/utils/local_storage_helper.dart';
 
-void main() {
+void main() async {
+  final LocalStorageHelper localStorageHelper = LocalStorageHelper();
+  await Hive.initFlutter();
+  localStorageHelper.registerAdapter();
+  await localStorageHelper.openBox();
   runApp(const MyApp());
 }
 
