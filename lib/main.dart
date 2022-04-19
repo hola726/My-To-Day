5cefc2b6f93ca93671f64ba71a5ee75ecb82e3db
jaeyun-park <wodbs720@naver.com>
2022-04-20 00:17:31 +0900

    Fix name init screen to main screen & Add listview

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:my_to_day/constants/constants.dart';
 import 'package:my_to_day/routes.dart';
-import 'package:my_to_day/screens/main/init_screen.dart';
+import 'package:my_to_day/screens/main/main_screen.dart';
 import 'package:my_to_day/utils/local_storage_helper.dart';
 
 void main() async {
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       builder: (BuildContext context) => MaterialApp(
         title: 'MyToDay',
         debugShowCheckedModeBanner: false,
-        initialRoute: InitScreen.id,
+        initialRoute: MainScreen.id,
         routes: routes,
         theme: ThemeData.light(),
       ),
