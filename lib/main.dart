62bb0eb72d70452213724008fe985dbf8cb12255
jaeyun-park <wodbs720@naver.com>
2022-04-21 23:08:05 +0900

    Add provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:my_to_day/constants/constants.dart';
+import 'package:my_to_day/provider/data_provider.dart';
 import 'package:my_to_day/routes.dart';
 import 'package:my_to_day/screens/main/main_screen.dart';
 import 'package:my_to_day/utils/local_storage_helper.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   final LocalStorageHelper localStorageHelper = LocalStorageHelper();
@@ -19,14 +21,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(kRatioBaseWith, kRatioBaseHeight),
-      builder: (BuildContext context) => MaterialApp(
-        title: 'MyToDay',
-        debugShowCheckedModeBanner: false,
-        initialRoute: MainScreen.id,
-        routes: routes,
-        theme: ThemeData.light(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<DataProvider>(
+          create: (_) => DataProvider(),
+        ),
+      ],
+      child: ScreenUtilInit(
+        designSize: const Size(kRatioBaseWith, kRatioBaseHeight),
+        builder: (BuildContext context) => MaterialApp(
+          title: 'MyToDay',
+          debugShowCheckedModeBanner: false,
+          initialRoute: MainScreen.id,
+          routes: routes,
+          theme: ThemeData.light(),
+        ),
       ),
     );
   }
