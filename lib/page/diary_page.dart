3f78599c7c5f4bb561f211499947bce16d289ec2
jaeyun-park <wodbs720@naver.com>
2022-05-06 22:23:24 +0900

    Add context option in diary provider

--- lib/page/diary_page.dart
+++ lib/page/diary_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_to_day/provider/data_provider.dart';
+import 'package:my_to_day/provider/diary_provider.dart';
 import 'package:my_to_day/screens/main/diary_screen.dart';
 import 'package:my_to_day/utils/local_storage_helper.dart';
 import 'package:provider/provider.dart';
@@ -10,9 +10,10 @@ class DiaryPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
+    return ChangeNotifierProvider<DiaryProvider>(
       create: (_) => DiaryProvider(
         localStorageHelper: LocalStorageHelper(),
+        context: context,
       ),
       child: DiaryScreen(),
     );
--- lib/provider/diary_provider.dart
+++ lib/provider/diary_provider.dart
@@ -1,33 +1,39 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:my_to_day/model/data/diary_data.dart';
 import 'package:my_to_day/utils/local_storage_helper.dart';
 
 class DiaryProvider extends ChangeNotifier {
   DiaryProvider({
-    required this.localStorageHelper,
-  }) {
+    required LocalStorageHelper localStorageHelper,
+    required BuildContext context,
+  })  : _localStorageHelper = localStorageHelper,
+        _context = context {
     getDiaryData();
   }
-  final LocalStorageHelper localStorageHelper;
+  final LocalStorageHelper _localStorageHelper;
+  final BuildContext _context;
 
   DiaryData? _diaryData;
   List<DiaryData>? _allDiaryData;
 
   DiaryData? get diaryData => _diaryData;
   List<DiaryData>? get allDiaryData => _allDiaryData;
+  void Function() get getDiaryData => _getDiaryData;
+  void Function() get gestureOnTap => _gestureOnTap;
 
   set diaryData(DiaryData? diaryData) {
     _diaryData = diaryData;
     notifyListeners();
   }
 
-  void getDiaryData() {
-    _allDiaryData = localStorageHelper.getAllDiaryData();
+  void _getDiaryData() {
+    _allDiaryData = _localStorageHelper.getAllDiaryData();
     notifyListeners();
   }
 
   void setDiaryData(String contents) {
-    localStorageHelper.setDiaryData(
+    _localStorageHelper.setDiaryData(
       date: DateTime.now().toString(),
       diaryDataModel: DiaryData(
         contents: contents,
@@ -40,5 +46,12 @@ class DiaryProvider extends ChangeNotifier {
     return data.time.year == previousData?.time.year &&
         data.time.month == previousData?.time.month &&
         data.time.day == previousData?.time.day;
+  }
+
+  void _gestureOnTap() {
+    FocusScopeNode currentFocus = FocusScope.of(_context);
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
   }
 }
