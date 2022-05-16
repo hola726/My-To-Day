ef3eef5bfa3016771a810e0e3dbfbff9d828063b
jaeyun-park <wodbs720@naver.com>
2022-05-17 00:21:34 +0900

    Add target data index, getDiaryData function in data provider

--- lib/provider/data_provider.dart
+++ lib/provider/data_provider.dart
@@ -6,7 +6,7 @@ class DataProvider extends ChangeNotifier {
   DataProvider({
     required LocalStorageHelper localStorageHelper,
   }) : _localStorageHelper = localStorageHelper {
-    getDiaryData();
+    getAllDiaryData();
   }
 
   final LocalStorageHelper _localStorageHelper;
@@ -14,13 +14,20 @@ class DataProvider extends ChangeNotifier {
   DiaryData? _diaryData;
   late List<DiaryData> _reversedData =
       _allDiaryData.reversed.map((data) => data).toList();
+  int _targetDataIndex = 0;
 
   DiaryData? get diaryData => _diaryData;
   List<DiaryData> get allDiaryData => _allDiaryData;
   List<DiaryData> get reversedData => _reversedData;
+  int get targetDataIndex => _targetDataIndex;
 
   set diaryData(DiaryData? diaryData) {
     _diaryData = diaryData;
+    notifyListeners();
+  }
+
+  set targetDataIndex(int targetDataIndex) {
+    _targetDataIndex = targetDataIndex;
     notifyListeners();
   }
 
@@ -29,10 +36,14 @@ class DataProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  void getDiaryData() {
+  void getAllDiaryData() {
     _allDiaryData = _localStorageHelper.getAllDiaryData();
     getReversedData();
     notifyListeners();
+  }
+
+  void getDiaryData() {
+    diaryData = _reversedData[_targetDataIndex];
   }
 
   void getReversedData() {
