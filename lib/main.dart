f8f358f3c3a56caf69a7b51c3ecd5606c24b1f98
jaeyun-park <wodbs720@naver.com>
2022-05-16 23:43:19 +0900

    Add data provider in main

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:my_to_day/constants/constants.dart';
+import 'package:my_to_day/provider/data_provider.dart';
 import 'package:my_to_day/routes.dart';
 import 'package:my_to_day/screens/main/diary_screen.dart';
 import 'package:my_to_day/utils/local_storage_helper.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   final LocalStorageHelper localStorageHelper = LocalStorageHelper();
   await Hive.initFlutter();
   localStorageHelper.registerAdapter();
   await localStorageHelper.openBox();
-  runApp(const MyApp());
+  runApp(
+    setProviders(
+      const MyApp(),
+    ),
+  );
+}
+
+Widget setProviders(Widget app) {
+  return MultiProvider(
+    providers: [
+      ChangeNotifierProvider<DataProvider>(
+        create: (_) => DataProvider(
+          localStorageHelper: LocalStorageHelper(),
+        ),
+      ),
+    ],
+    child: app,
+  );
 }
 
 class MyApp extends StatelessWidget {
