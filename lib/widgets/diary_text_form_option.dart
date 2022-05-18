697727d16bd6aa6e2737c6fb5a26bd9decc56afb
jaeyun-park <wodbs720@naver.com>
2022-05-18 23:25:23 +0900

    Add open delete text modal in diary text form option

--- lib/widgets/diary_text_form_option.dart
+++ lib/widgets/diary_text_form_option.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/src/size_extension.dart';
+import 'package:my_to_day/constants/constant_strings.dart' as CS;
 import 'package:my_to_day/provider/diary_provider.dart';
 import 'package:my_to_day/utils/date_helper.dart';
 
 import '../app_theme.dart';
+import 'common/contour.dart';
 
 class DiaryTextFormOption extends StatelessWidget {
   const DiaryTextFormOption({
@@ -13,6 +15,79 @@ class DiaryTextFormOption extends StatelessWidget {
         super(key: key);
 
   final DiaryProvider _diaryProvider;
+
+  Future<void> openDeleteTextModal() async {
+    await showModalBottomSheet(
+        context: _diaryProvider.context,
+        backgroundColor: AppTheme.textSecondary2Color,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+        ),
+        builder: (BuildContext context) {
+          return SizedBox(
+            height: 120.h,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '삭제 하시겠습니까?',
+                  style: AppTheme.basicInterTextStyle
+                      .copyWith(color: AppTheme.grey500),
+                ),
+                Contour(
+                  color: AppTheme.grey500,
+                  padding: EdgeInsets.symmetric(
+                    vertical: 5.h,
+                  ),
+                  height: 1.h,
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.h),
+                  child: InkWell(
+                    onTap: () {
+                      _diaryProvider.textEditingController.clear();
+                      Navigator.of(context).pop();
+                    },
+                    splashColor: Colors.transparent,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: Center(
+                        child: Text(
+                          CS.YES,
+                          style: AppTheme.errorTextStyle.copyWith(
+                            fontSize: 16.sp,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.h),
+                  child: InkWell(
+                      onTap: Navigator.of(context).pop,
+                      splashColor: Colors.transparent,
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: Center(
+                          child: Text(
+                            CS.CANCEL,
+                            style: TextStyle(
+                              fontSize: 16.sp,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ),
+                      )),
+                ),
+              ],
+            ),
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +127,9 @@ class DiaryTextFormOption extends StatelessWidget {
             ),
           ),
           IconButton(
-            onPressed: () => {},
+            onPressed: _diaryProvider.textEditingController.text.isNotEmpty
+                ? openDeleteTextModal
+                : null,
             icon: const Icon(
               Icons.restore_from_trash_outlined,
               color: Colors.white,
