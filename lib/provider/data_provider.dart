53129c3744d71c5ba5717d3f400dc1d573921274
jaeyun-park <wodbs720@naver.com>
2022-05-19 23:28:56 +0900

    Add filteredData in data provider

--- lib/provider/data_provider.dart
+++ lib/provider/data_provider.dart
@@ -11,14 +11,20 @@ class DataProvider extends ChangeNotifier {
 
   final LocalStorageHelper _localStorageHelper;
   late List<DiaryData> _allDiaryData;
+  List<DiaryData?> _filteredDiaryData = [];
   DiaryData? _diaryData;
   late List<DiaryData> _reversedData =
       _allDiaryData.reversed.map((data) => data).toList();
+
+  List<DiaryData?> _filteredReversedData = [];
+
   int _targetDataIndex = 0;
 
   DiaryData? get diaryData => _diaryData;
   List<DiaryData> get allDiaryData => _allDiaryData;
+  List<DiaryData?> get filteredDiaryData => _filteredDiaryData;
   List<DiaryData> get reversedData => _reversedData;
+  List<DiaryData?> get filteredReversedData => _filteredReversedData;
   int get targetDataIndex => _targetDataIndex;
 
   set diaryData(DiaryData? diaryData) {
@@ -48,5 +54,24 @@ class DataProvider extends ChangeNotifier {
 
   void getReversedData() {
     _reversedData = _allDiaryData.reversed.map((data) => data).toList();
+  }
+
+  void filteredData(String value) {
+    print(value);
+    if (value == '') {
+      print('in');
+      _filteredDiaryData = [];
+      _filteredReversedData = [];
+      notifyListeners();
+    }
+
+    _filteredDiaryData = _allDiaryData.map((diaryData) {
+      if (diaryData.contents.contains(value)) {
+        return diaryData;
+      }
+    }).toList();
+    _filteredReversedData =
+        _filteredDiaryData.reversed.map((data) => data).toList();
+    notifyListeners();
   }
 }
