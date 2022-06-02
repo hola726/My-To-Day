9e47823090306eb63a16b61e67a858ff623e98b9
jaeyun-park <wodbs720@naver.com>
2022-06-02 23:32:44 +0900

    Remove logic in diary text form option

--- lib/widgets/diary_text_form_option.dart
+++ lib/widgets/diary_text_form_option.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:my_to_day/constants/constant_strings.dart' as CS;
-import 'package:my_to_day/model/data/diary_data.dart';
 import 'package:my_to_day/provider/data_provider.dart';
 import 'package:my_to_day/provider/diary_provider.dart';
 import 'package:my_to_day/utils/date_helper.dart';
@@ -16,14 +14,14 @@ class DiaryTextFormOption extends StatelessWidget {
     required DiaryProvider diaryProvider,
     required DataProvider dataProvider,
     bool? isEditTextFormOption,
-  })  : _isEdit = isEditTextFormOption,
+  })  : _isEditTextFormOption = isEditTextFormOption,
         _diaryProvider = diaryProvider,
         _dataProvider = dataProvider,
         super(key: key);
 
   final DiaryProvider _diaryProvider;
   final DataProvider _dataProvider;
-  final bool? _isEdit;
+  final bool? _isEditTextFormOption;
 
   Future<void> openDeleteTextModal() async {
     await showModalBottomSheet(
@@ -98,70 +96,6 @@ class DiaryTextFormOption extends StatelessWidget {
         });
   }
 
-  void setPhoto(XFile? photo) {
-    if (_dataProvider.tmpDiaryData != null) {
-      _dataProvider.tmpDiaryData = _dataProvider.tmpDiaryData!.copyWith(
-        cameraImage: photo,
-      );
-    } else {
-      _dataProvider.tmpDiaryData = DiaryData(
-        contents: '',
-        time: DateTime.now(),
-        cameraImage: photo,
-      );
-    }
-  }
-
-  void editPhoto(XFile? photo) {
-    if (_dataProvider.diaryData != null) {
-      _dataProvider.diaryData = _dataProvider.diaryData!.copyWith(
-        cameraImage: photo,
-      );
-    }
-  }
-
-  void setPickerImages(List<XFile>? images) {
-    if (_dataProvider.tmpDiaryData != null) {
-      _dataProvider.tmpDiaryData = _dataProvider.tmpDiaryData!.copyWith(
-        pickerImages: images,
-      );
-    } else {
-      _dataProvider.tmpDiaryData = DiaryData(
-        contents: '',
-        time: DateTime.now(),
-        pickerImages: images,
-      );
-    }
-  }
-
-  void editPickerImages(List<XFile>? images) {
-    if (_dataProvider.diaryData != null) {
-      _dataProvider.diaryData = _dataProvider.diaryData!.copyWith(
-        pickerImages: images,
-      );
-    }
-  }
-
-  Color setPhotoColor() {
-    return _isEdit == true
-        ? _dataProvider.diaryData?.cameraImage != null
-            ? Colors.red
-            : Colors.white
-        : _dataProvider.tmpDiaryData?.cameraImage != null
-            ? Colors.red
-            : Colors.white;
-  }
-
-  Color setPickerImageColor() {
-    return _isEdit == true
-        ? _dataProvider.diaryData?.pickerImages != null
-            ? Colors.red
-            : Colors.white
-        : _dataProvider.tmpDiaryData?.pickerImages != null
-            ? Colors.red
-            : Colors.white;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -170,35 +104,18 @@ class DiaryTextFormOption extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           IconButton(
-            onPressed: () async {
-              final ImagePicker _picker = ImagePicker();
-              final XFile? photo =
-                  await _picker.pickImage(source: ImageSource.camera);
-              if (_isEdit == true) {
-                editPhoto(photo);
-              } else {
-                setPhoto(photo);
-              }
-            },
+            onPressed: () => _dataProvider.getPhoto(_isEditTextFormOption),
             icon: Icon(
               Icons.camera_alt_outlined,
-              color: setPhotoColor(),
+              color: _dataProvider.setPhotoColor(_isEditTextFormOption),
             ),
           ),
           IconButton(
-            onPressed: () async {
-              final ImagePicker _picker = ImagePicker();
-              final List<XFile>? images = await _picker.pickMultiImage();
-
-              if (_isEdit == true) {
-                editPickerImages(images);
-              } else {
-                setPickerImages(images);
-              }
-            },
+            onPressed: () =>
+                _dataProvider.getPickerImages(_isEditTextFormOption),
             icon: Icon(
               Icons.add_photo_alternate_outlined,
-              color: setPickerImageColor(),
+              color: _dataProvider.setPickerImageColor(_isEditTextFormOption),
             ),
           ),
           IconButton(
