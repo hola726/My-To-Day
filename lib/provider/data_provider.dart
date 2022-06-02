4a288e50901948e12ab04f0bd9e6f27579346135
jaeyun-park <wodbs720@naver.com>
2022-06-02 23:27:18 +0900

    Add get photo, get picker images ini data provider

--- lib/provider/data_provider.dart
+++ lib/provider/data_provider.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:my_to_day/model/data/diary_data.dart';
 import 'package:my_to_day/utils/local_storage_helper.dart';
 
@@ -20,10 +22,15 @@ class DataProvider extends ChangeNotifier {
   String? _filteredValue;
 
   DiaryData? get diaryData => _diaryData;
+
   DiaryData? get tmpDiaryData => _tmpDiaryData;
+
   List<DiaryData> get allDiaryData => _allDiaryData;
+
   List<DiaryData> get reversedData => _reversedData;
+
   List<DiaryData?> get filteredReversedData => _filteredReversedData;
+
   int get targetDataIndex => _targetDataIndex;
 
   set diaryData(DiaryData? diaryData) {
@@ -93,5 +100,84 @@ class DataProvider extends ChangeNotifier {
         .map((data) => data)
         .toList();
     notifyListeners();
+  }
+
+  double handleFillImageHeight() {
+    return (tmpDiaryData?.cameraImage != null ||
+            tmpDiaryData?.pickerImages != null)
+        ? 166.h
+        : 116.h;
+  }
+
+  double handleEditImageHeight() {
+    return (_diaryData?.cameraImage != null || _diaryData?.pickerImages != null)
+        ? 166.h
+        : 116.h;
+  }
+
+  void _editPhoto(XFile? photo) {
+    if (_diaryData != null) {
+      print('edit photo');
+      diaryData = _diaryData!.copyWith(
+        cameraImage: photo,
+      );
+    }
+  }
+
+  void _setPhoto(XFile? photo) {
+    if (_tmpDiaryData != null) {
+      tmpDiaryData = _tmpDiaryData!.copyWith(
+        cameraImage: photo,
+      );
+    } else {
+      tmpDiaryData = DiaryData(
+        contents: '',
+        time: DateTime.now(),
+        cameraImage: photo,
+      );
+    }
+  }
+
+  void getPhoto(bool? isEdit) async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+    if (isEdit == true) {
+      _editPhoto(photo);
+    } else {
+      _setPhoto(photo);
+    }
+  }
+
+  void _setPickerImages(List<XFile>? images) {
+    if (_tmpDiaryData != null) {
+      tmpDiaryData = _tmpDiaryData!.copyWith(
+        pickerImages: images,
+      );
+    } else {
+      _tmpDiaryData = DiaryData(
+        contents: '',
+        time: DateTime.now(),
+        pickerImages: images,
+      );
+    }
+  }
+
+  void _editPickerImages(List<XFile>? images) {
+    if (_diaryData != null) {
+      diaryData = _diaryData!.copyWith(
+        pickerImages: images,
+      );
+    }
+  }
+
+  void getPickerImages(bool? isEdit) async {
+    final ImagePicker _picker = ImagePicker();
+    final List<XFile>? images = await _picker.pickMultiImage();
+
+    if (isEdit == true) {
+      _editPickerImages(images);
+    } else {
+      _setPickerImages(images);
+    }
   }
 }
