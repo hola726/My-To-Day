7e29bbb38d72638ab69e4de267b902caafef6d93
jaeyun-kmov <jaeyun.park@ssvt.co.kr>
2022-06-14 23:38:27 +0900

    Add selectedDay, focusedDay in calendar provider

--- lib/provider/calendar_provider.dart
+++ lib/provider/calendar_provider.dart
@@ -2,4 +2,24 @@ import 'package:flutter/material.dart';
 
 class CalendarProvider extends ChangeNotifier {
   CalendarProvider();
+
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
+
+  DateTime get selectedDay => _selectedDay;
+  DateTime get focusedDay => _focusedDay;
+
+  set selectedDay(DateTime selectedDay) {
+    _selectedDay = selectedDay;
+    notifyListeners();
+  }
+
+  set focusedDay(DateTime focusedDay) {
+    _focusedDay = focusedDay;
+    notifyListeners();
+  }
+
+  bool isSameDay(DateTime firstDay, DateTime secondDay) {
+    return firstDay == secondDay;
+  }
 }
