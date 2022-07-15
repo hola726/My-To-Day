dc80d3a38827bc2176b16f20c421f33bd4107922
Park Jae Yun <wodbs720@naver.com>
2022-07-15 17:08:47 +0900

    Add context in calendar provider

--- lib/provider/calendar_provider.dart
+++ lib/provider/calendar_provider.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class CalendarProvider extends ChangeNotifier {
-  CalendarProvider();
-
+  CalendarProvider({
+    required BuildContext context,
+  }) : _context = context;
+  final BuildContext _context;
   DateTime _selectedDay = DateTime.now();
   DateTime _focusedDay = DateTime.now();
 
   DateTime get selectedDay => _selectedDay;
   DateTime get focusedDay => _focusedDay;
+  BuildContext get context => _context;
 
   set selectedDay(DateTime selectedDay) {
     _selectedDay = selectedDay;
