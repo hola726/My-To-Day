b5f1159b9bfe30435d1d682e015c642107b6b01e
Jae Yun Park <wodbs720@naver.com>
2023-04-11 08:00:49 +0900

    fix( diary_edit_page_model.dart ): 버튼 로직 추가

--- lib/feature/diary/page_model/diary_edit_page_model.dart
+++ lib/feature/diary/page_model/diary_edit_page_model.dart
@@ -1,73 +1,125 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:my_to_day/feature/diary/service/diary_local_service.dart';
+import 'package:path_provider/path_provider.dart';
 
 import '../../../model/data/diary_data.dart';
-import '../../../provider/data_provider.dart';
 
 class DiaryEditPageModel extends ChangeNotifier {
   DiaryEditPageModel({
     required BuildContext context,
-    required DataProvider dataProvider,
+    required DiaryData diaryData,
     required FocusNode diaryTextFormFocusNode,
     required DiaryLocalService diaryLocalService,
     required TextEditingController diaryTextFormController,
   })  : _context = context,
-        _dataProvider = dataProvider,
-        _diaryLocalService = diaryLocalService,
+        _diaryData = diaryData,
+        _localService = diaryLocalService,
         _diaryTextFormFocusNode = diaryTextFormFocusNode,
         _diaryTextFormController = diaryTextFormController {
     init();
   }
 
   final BuildContext _context;
-  final DataProvider _dataProvider;
   final FocusNode _diaryTextFormFocusNode;
-  final DiaryLocalService _diaryLocalService;
+  final DiaryLocalService _localService;
   final TextEditingController _diaryTextFormController;
+  DiaryData _diaryData;
 
   BuildContext get context => _context;
-  DataProvider get dataProvider => _dataProvider;
+  DiaryData get diaryData => _diaryData;
   TextEditingController get diaryTextFormController => _diaryTextFormController;
   FocusNode get diaryTextFormFocusNode => _diaryTextFormFocusNode;
+
 
   void init() {
     _diaryTextFormFocusNode.requestFocus();
   }
 
-  Future<void> editDiaryData({
-    required String contents,
-    required DateTime date,
-    String? cameraImage,
-    List<String>? pickerImage,
-    String? locate,
-  }) async {
-    await _diaryLocalService.editDiaryData(
-      key: date.toString(),
-      diaryDataModel: DiaryData(
-        contents: contents,
-        time: date,
-        cameraImage: cameraImage,
-        pickerImages: pickerImage,
-        locate: locate,
-      ),
-    );
-  }
-
   void onEditPressed() async {
-    await editDiaryData(
-      contents: _diaryTextFormController.text,
-      date: _dataProvider.diaryData!.time,
-      cameraImage: _dataProvider.diaryData?.cameraImage,
-      pickerImage: _dataProvider.diaryData?.pickerImages,
-      locate: _dataProvider.diaryData?.locate,
+    await _localService.editDiaryData(
+      key: _diaryData.time.toString(),
+      diaryDataModel:
+          _diaryData.copyWith(contents: _diaryTextFormController.text),
     );
-    _dataProvider.getAllDiaryData();
+
     _diaryTextFormController.clear();
-    Navigator.of(_context).pop();
+    _context.pop();
   }
 
   void onLeadingPressed() {
-    _context.pop;
+    _context.pop();
+  }
+
+  void _setPhoto(String? photo) {
+    _diaryData = _diaryData.copyWith(
+      cameraImage: photo,
+    );
+    notifyListeners();
+  }
+
+  void onCameraPressed() async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+
+    if (photo != null) {
+      final String path = (await getApplicationDocumentsDirectory()).path;
+
+      String localPath = "$path/${photo.name}";
+      await File(photo.path).copy(localPath);
+
+      String photoString = photo.name;
+
+      _setPhoto(photoString);
+    }
+  }
+
+  void _setPickerImages(List<String>? images) {
+    _diaryData = _diaryData.copyWith(
+      pickerImages: images,
+    );
+    notifyListeners();
+  }
+
+  void onImagesPressed() async {
+    final ImagePicker _picker = ImagePicker();
+    final List<XFile>? images = await _picker.pickMultiImage();
+    if (images == null) return;
+
+    final String path = (await getApplicationDocumentsDirectory()).path;
+
+    List<String>? imageStrings;
+
+    for (XFile image in images) {
+      String localPath = "$path/${image.name}";
+
+      await File(image.path).copy(localPath);
+
+      if (imageStrings == null) {
+        imageStrings = [image.name];
+      } else {
+        imageStrings.add(image.name);
+      }
+    }
+
+    _setPickerImages(imageStrings);
+  }
+
+  void onDeletePressed() {
+    // todo : 삭제 기능 구현
+    // _diaryTextFormController.clear();
+    // _diaryData = null;
+    // _context.pop();
+    // notifyListeners();
+  }
+
+  double handleEditImageHeight() {
+    return (_diaryData.cameraImage != null || _diaryData.pickerImages != null)
+        ? 166.h
+        : 116.h;
   }
 }
