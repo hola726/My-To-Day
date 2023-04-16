8f9b12b64805c4c3a5a810787e0cb656ce79066e
Jae Yun Park <wodbs720@naver.com>
2023-04-17 06:40:00 +0900

    fix( diary_item.dart ): 더보기 버튼 삭제

--- lib/widgets/common/diary_item.dart
+++ lib/widgets/common/diary_item.dart
@@ -4,47 +4,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:insta_image_viewer/insta_image_viewer.dart';
 import 'package:my_to_day/model/data/diary_data.dart';
-import 'package:my_to_day/utils/date_helper.dart';
-import 'package:path_provider/path_provider.dart';
 
 import '../../app_theme.dart';
+import '../../utils/date_helper.dart';
 
-class DiaryItem extends StatefulWidget {
+class DiaryItem extends StatelessWidget {
   const DiaryItem({
     Key? key,
     required this.data,
+    required this.localPath,
     this.onTap,
   }) : super(key: key);
 
   final DiaryData data;
+  final String localPath;
   final void Function()? onTap;
 
-  @override
-  State createState() => _DiaryItemState();
-}
-
-class _DiaryItemState extends State<DiaryItem> {
-  bool _isViewMore = false;
-  String? _localPath;
-
-  late final bool _isLongLineContents = widget.data.contents.length > 93;
-
-  @override
-  void initState() {
-    super.initState();
-    init();
-  }
-
-  void init() async {
-    _localPath = (await getApplicationDocumentsDirectory()).path;
-  }
-
   String _getPath() {
-    if (widget.data.cameraImage != null) {
-      return "$_localPath/${widget.data.cameraImage}";
+    if (data.cameraImage != null) {
+      return "$localPath/${data.cameraImage}";
     }
 
-    return "$_localPath/${widget.data.pickerImages![0]}";
+    return "$localPath/${data.pickerImages![0]}";
   }
 
   @override
@@ -54,7 +35,7 @@ class _DiaryItemState extends State<DiaryItem> {
         vertical: 3.h,
       ),
       child: InkWell(
-        onTap: widget.onTap,
+        onTap: onTap,
         child: Container(
           width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
@@ -71,41 +52,24 @@ class _DiaryItemState extends State<DiaryItem> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        widget.data.contents,
-                        maxLines: _isViewMore ? null : 3,
+                        data.contents,
+                        maxLines: 3,
                         style: AppTheme.button_small_KR.copyWith(
                           color: Colors.white,
                         ),
                       ),
-                      if (!_isLongLineContents || _isViewMore == true)
-                        Text(
-                          DateHelper.convertDateAmPm(widget.data.time),
-                          style: AppTheme.button_small_KR.copyWith(
-                            color: AppTheme.grey400,
-                            fontSize: 11.sp,
-                          ),
+                      Text(
+                        DateHelper.convertDateAmPm(data.time),
+                        style: AppTheme.button_small_KR.copyWith(
+                          color: AppTheme.grey400,
+                          fontSize: 11.sp,
                         ),
-                      if (_isLongLineContents)
-                        InkWell(
-                          onTap: () => setState(() {
-                            _isViewMore = !_isViewMore;
-                          }),
-                          child: Text(
-                            _isViewMore ? '접기' : '더보기',
-                            style: AppTheme.button_small_KR.copyWith(
-                              decoration: TextDecoration.underline,
-                              color: AppTheme.grey400,
-                              fontSize: 11.sp,
-                            ),
-                          ),
-                        ),
+                      ),
                     ],
                   ),
                 ),
                 SizedBox(width: 10.w),
-                if ((widget.data.cameraImage != null ||
-                        widget.data.pickerImages != null) &&
-                    _localPath != null)
+                if ((data.cameraImage != null || data.pickerImages != null))
                   SizedBox(
                     width: 40.w,
                     height: 40.h,
