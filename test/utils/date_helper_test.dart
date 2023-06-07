d52eaa8b5a952a8e5bab35b2694a178bcd449b40
Jae Yun Park <wodbs720@naver.com>
2023-06-07 16:04:20 +0900

    feat( date_helper_test.dart ): 날짜 테스트 추가

--- test/utils/date_helper_test.dart
+++ test/utils/date_helper_test.dart
@@ -2,18 +2,43 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:my_to_day/utils/date_helper.dart';
 
 void main() {
-  test("should convert date", () {
-    String convertDate = DateHelper.convertDate(DateTime(2023, 01, 01, 01, 01));
-    expect(convertDate, "01/01 AM 01:01");
+  group("convert date", () {
+    test("should convert date", () {
+      String convertDate =
+          DateHelper.convertDate(DateTime(2023, 01, 01, 01, 01));
+      expect(convertDate, "01/01 AM 01:01");
+    });
+    test("should convert date", () {
+      String convertDate =
+          DateHelper.convertDate(DateTime(2024, 02, 02, 02, 02));
+      expect(convertDate, "02/02 AM 02:02");
+    });
   });
-  test("should convert date am pm", () {
-    String convertDate =
-        DateHelper.convertDateAmPm(DateTime(2023, 01, 01, 01, 01));
-    expect(convertDate, "AM 01:01");
+
+  group("convert date am pm", () {
+    test("should convert date am pm", () {
+      String convertDate =
+          DateHelper.convertDateAmPm(DateTime(2023, 01, 01, 01, 01));
+      expect(convertDate, "AM 01:01");
+    });
+
+    test("should convert date am pm", () {
+      String convertDate =
+          DateHelper.convertDateAmPm(DateTime(2024, 02, 02, 02, 02));
+      expect(convertDate, "AM 02:02");
+    });
   });
-  test("should convert date Month", () {
-    String convertDate =
-        DateHelper.convertDateMonth(DateTime(2023, 01, 01, 01, 01));
-    expect(convertDate, "1 Jan, 2023");
+
+  group("convert date Month", () {
+    test("should convert date Month", () {
+      String convertDate =
+          DateHelper.convertDateMonth(DateTime(2023, 01, 01, 01, 01));
+      expect(convertDate, "1 Jan, 2023");
+    });
+    test("should convert date Month", () {
+      String convertDate =
+          DateHelper.convertDateMonth(DateTime(2024, 02, 02, 02, 02));
+      expect(convertDate, "2 Feb, 2024");
+    });
   });
 }
