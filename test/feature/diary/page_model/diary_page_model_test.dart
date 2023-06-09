eb6ebbf6fe3b00320ad9184ef20afc0b3edeef99
Jae Yun Park <wodbs720@naver.com>
2023-06-09 11:25:58 +0900

    feat( diary_page_model_test.dart ): 테스트 추가

--- test/feature/diary/page_model/diary_page_model_test.dart
+++ test/feature/diary/page_model/diary_page_model_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:my_to_day/feature/diary/page_model/diary_page_model.dart';
+import 'package:my_to_day/model/data/diary_data.dart';
 import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 
@@ -17,6 +18,21 @@ class FakePathProviderPlatform extends Fake
     return kApplicationDocumentsPath;
   }
 }
+
+final List<DiaryData> mockDiaryDataList = [
+  DiaryData(
+    contents: "contents",
+    time: DateTime(2023, 01, 01),
+  ),
+  DiaryData(
+    contents: "value",
+    time: DateTime(2024, 02, 02),
+  ),
+  DiaryData(
+    contents: "value",
+    time: DateTime(2025, 03, 03),
+  ),
+];
 
 void main() {
   late DiaryPageModel diaryPageModel;
@@ -37,20 +53,97 @@ void main() {
     );
   });
 
-  test("should edit diary data", () {
-    // given
-    when(mockDiaryLocalService.editDiaryData(diaryData: anyNamed("diaryData")))
-        .thenAnswer((realInvocation) async {});
+  group("diary page model", () {
+    test("should edit diary data", () {
+      // given
+      when(mockDiaryLocalService.editDiaryData(
+              diaryData: anyNamed("diaryData")))
+          .thenAnswer((realInvocation) async {});
 
-    // when
-    diaryPageModel.editDiaryData(
-      contents: "contents",
-      date: DateTime(2023, 01, 01),
-    );
+      // when
+      diaryPageModel.editDiaryData(
+        contents: "contents",
+        date: DateTime(2023, 01, 01),
+      );
 
-    // then
-    verify(mockDiaryLocalService.editDiaryData(
-            diaryData: anyNamed("diaryData")))
-        .called(1);
+      // then
+      verify(mockDiaryLocalService.editDiaryData(
+              diaryData: anyNamed("diaryData")))
+          .called(1);
+    });
+
+    test("should handle search text form changed", () {
+      // given
+      when(mockTextEditingController.value)
+          .thenReturn(TextEditingValue(text: "text"));
+
+      // when
+      diaryPageModel.handleSearchedDataChanged("value");
+
+      // then
+      expect(mockTextEditingController.value.text, "text");
+    });
+
+    group("get all diary data", () {
+      test("should get all diary data check diary data list", () {
+        // given
+        clearInteractions(mockDiaryLocalService);
+        when(mockDiaryLocalService.getAllDiaryData())
+            .thenReturn(mockDiaryDataList);
+        List<DiaryData> handleDiaryDataList = mockDiaryDataList;
+        handleDiaryDataList.sort((a, b) {
+          return a.time.compareTo(b.time);
+        });
+        handleDiaryDataList = handleDiaryDataList.reversed.toList();
+
+        // when
+        diaryPageModel.getAllDiaryData();
+
+        // then
+        verify(mockDiaryLocalService.getAllDiaryData()).called(1);
+
+        expect(mockDiaryLocalService.getAllDiaryData(), mockDiaryDataList);
+        expect(diaryPageModel.diaryDataList, handleDiaryDataList);
+      });
+
+      test("should get all diary data check searched data", () {
+        // when
+        diaryPageModel.getAllDiaryData();
+
+        // then
+        expect(diaryPageModel.searchedData, []);
+      });
+    });
+
+    group("handle searched data changed", () {
+      test("should searched data changed", () {
+        //given
+        when(mockDiaryLocalService.getAllDiaryData())
+            .thenReturn(mockDiaryDataList);
+
+        //when
+        diaryPageModel.getAllDiaryData();
+        diaryPageModel.handleSearchedDataChanged("value");
+
+        // then
+        expect(diaryPageModel.searchedData.length, 2);
+        expect(diaryPageModel.searchedData[0].time, mockDiaryDataList[2].time);
+        expect(diaryPageModel.searchedData[1].time, mockDiaryDataList[1].time);
+      });
+
+      test("should searched data empty", () {
+        //given
+        when(mockDiaryLocalService.getAllDiaryData())
+            .thenReturn(mockDiaryDataList);
+
+        //when
+        diaryPageModel.getAllDiaryData();
+        diaryPageModel.handleSearchedDataChanged("");
+
+        // then
+        expect(diaryPageModel.searchedData.length, 0);
+        expect(diaryPageModel.searchedData.isEmpty, true);
+      });
+    });
   });
 }
